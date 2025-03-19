6d766fba2f01a58012bd2baf06d14bd5c9bb9c82
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2025-03-19 18:00:09 -0400

    adding basic java files

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello
+{
+    public static void main(String a[])
+        {
+            int  num1 = 5;
+            int  num2 =10;
+            int  result = num1 + num2;
+        System.out.println(result);
+        }
+}   
---
+++ relational.java
@@ -0,0 +1,11 @@
+public class relational
+{
+    public static void main(String a[])
+        {
+            int  x = 6;
+            int  y = 7;
+            
+            boolean result = x < y;
+        System.out.println(result);
+        }
+}   
